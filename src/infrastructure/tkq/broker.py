580c3d0e5b033aeb31c8ebbfb2399970794042cd
o-l-e-z-a <oleg999000@yandex.ru>
2025-05-21 16:17:29 +0300

    fix (infra): remove useless local files from git

--- src/infrastructure/tkq/broker.py
+++ src/infrastructure/tkq/broker.py
@@ -3,19 +3,13 @@ import taskiq_fastapi
 from taskiq_aio_pika import AioPikaBroker
 
 from src.infrastructure.logger_adapter.logger import init_logger
+from src.presentation.api.settings import settings
 
-# from src.presentation.api.settings import settings
-
-# config = settings.rabbit
+config = settings.rabbit
 logger = init_logger(__name__)
 
 
-# config_str = f'amqp://{config.RABBIT_USER}:{config.RABBIT_PASS}@{config.RABBIT_HOST}:{config.RABBIT_PORT}'
-taskiq_broker = AioPikaBroker("amqp://guest:guest@localhost:5672")
-
-
-# taskiq_broker = InMemoryBroker()
-
+url = f"amqp://{config.RABBIT_USER}:{config.RABBIT_PASS}@{config.RABBIT_HOST}:{config.RABBIT_PORT}"
+taskiq_broker = AioPikaBroker(url)
 
 taskiq_fastapi.init(taskiq_broker, "src.presentation.api.main:create_fastapi_app")
-# taskiq_fastapi.init(taskiq_broker, "src.presentation.api.main:app")
